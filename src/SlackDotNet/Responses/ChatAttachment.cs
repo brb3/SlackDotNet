5a009d035654e25e149381ef5981557e299e29c7
Bobby Burden III <bobby@brb3.org>
2022-03-27 18:25:56 -0400

    clean slate

--- src/SlackDotNet/Responses/ChatAttachment.cs
+++
@@ -1,9 +0,0 @@
-namespace SlackDotNet.Responses
-{
-    public class ChatAttachment
-    {
-        public string Text { get; set; }
-        public int Id { get; set; }
-        public string Fallback { get; set; }
-    }
-}
